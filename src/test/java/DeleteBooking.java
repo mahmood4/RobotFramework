4e82bbb80623868936610e6e2cd6c58a8892bc49
mahmood4 <mahmood4@gmail.com>
2022-11-28 22:00:15 -0500

    Delete_Param

--- src/test/java/DeleteBooking.java
+++ src/test/java/DeleteBooking.java
@@ -3,10 +3,14 @@ import org.testng.annotations.Test;
 import utility.AllureLogger;
 import utility.BaseTest;
 
+import java.io.IOException;
+import java.util.Properties;
+
 import static io.restassured.RestAssured.given;
 
 public class DeleteBooking extends BaseTest {
 
+	public static Properties prop;
 	@Test(description="To delete the details of the booking IDs") 
 	public void getBookingIDs(){
 		
@@ -24,15 +28,9 @@ public class DeleteBooking extends BaseTest {
 		CreateBooking createBooking = new CreateBooking();
 		createBooking.createNewBooking("Jon", "114", "2018-01-05","null");
 
-
-
-
 		String IDtoUpdate = createBooking.newID;
 		AllureLogger.logToAllure("New Booking ID created is : "+IDtoUpdate);
 		AllureLogger.logToAllure("Booking ID to be deleted is : "+IDtoUpdate);
-
-
-
 
 		//Sending the GET request for a specific booking id and receiving the response
 		AllureLogger.logToAllure("Sending the GET request for a specific booking id and receiving the response");
@@ -52,10 +50,17 @@ public class DeleteBooking extends BaseTest {
 		logResponseAsString(response);	
 		
 	}
-	@Test(enabled = false,description="To delete the details of the Exsiting booking IDs")
-	public void getExistingBookingIDs(){
+	@Test(enabled = true,description="To delete the details of the Exsiting booking IDs")
+	public void getExistingBookingIDs() throws IOException {
 
 		AllureLogger.logToAllure("Starting the test to delete booking details");
+
+		prop = new Properties();
+
+		prop.load(DeleteBooking.class.getClassLoader().getResourceAsStream("ApiParam.properties"));
+		System.out.println("ID to Delete    "+prop.getProperty("IdToDel"));
+		//String browserName=prop.getProperty("browserVal");
+
 		/*******************************************************
 		 * Send a DELETE request to /booking/{id}
 		 * and check that the response has HTTP status code 200
@@ -71,7 +76,7 @@ public class DeleteBooking extends BaseTest {
 				spec(requestSpec).
 				header("Content-Type", "application/json").
 				//	header("Cookie", cookieValue).
-						pathParam("id", 16).
+						pathParam("id", prop.getProperty("IdToDel")).
 				when().
 				delete("/posts/{id}");
 
